6fd5f3f9e43ff7a943483ebea8cd78dff587a3e3
Sparshh27 <97667061+Sparshh27@users.noreply.github.com>
2022-08-22 23:19:45 +0530

    Create circulartour.cpp
    GFG code for circular tour.

---
+++ circulartour.cpp
@@ -0,0 +1,31 @@
+# DSA-Codes
+class Solution
+{
+    int tour(int petrol[], int distance[])
+    {
+	   int start = 0 ;
+	   int deficit = 0;
+	   int balance = 0;
+	   
+	   for(int i = 0 ; i < petrol.length ; i++)
+	   {
+	        balance = balance + petrol[i] - distance[i];
+	        
+	        if( balance < 0)
+	        {
+	            deficit = deficit + balance;
+	            start = i+1;
+	            balance = 0;
+	        }
+	   }
+
+	   if( deficit + balance >= 0){
+	       
+	       return start;
+	   }
+	   
+	   else
+	   return -1;
+	   
+    }
+}
